f7f9f5643649d35243f5efcbfd6d3e7cf803d8e3
Tim Zimmermann <tim@linux4.de>
2020-09-20 11:15:39 +0200

    gts7fewifi: configs: Add config for spen
    * Fixes spen in landscape orientation
    
    Change-Id: Icf9ec6efb1e04d150320d2016ee687c8ff3da97e

--- device.mk
+++ device.mk
@@ -44,6 +44,7 @@ PRODUCT_COPY_FILES += \
 
 # Input configs
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/idc/sec_e-pen.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/sec_e-pen.idc \
     $(LOCAL_PATH)/configs/idc/Vendor_04e8_Product_a035.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/Vendor_04e8_Product_a035.idc
 
 # Sensors
