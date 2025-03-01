196945d5a73f0ca8720298b6211dbd456c334c17
Bush-cat <73399885+Bush-cat@users.noreply.github.com>
2025-03-01 22:23:59 +0000

    move prop to makefile

--- lineage_gts7fewifi.mk
+++ lineage_gts7fewifi.mk
@@ -33,6 +33,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 TARGET_SCREEN_HEIGHT := 1600
 TARGET_SCREEN_WIDTH := 2560
 
+# Display
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.primary_display_orientation=ORIENTATION_90
+
 TARGET_IS_TABLET := true
 PRODUCT_CHARACTERISTICS := tablet
 
